884ac02cb01eb72d4065021ab0462244f7f321fa
Abduallah Mohsen <42982063+xAbduallah@users.noreply.github.com>
2024-05-15 15:27:27 +0300

    Add files via upload

---
+++ 98Fix.bat
@@ -0,0 +1,19 @@
+taskkill /f /t /im adb.exe
+taskkill /f /t /im AppMarket.exe
+taskkill /f /t /im AndroidEmulatorEx.exe
+taskkill /f /t /im AndroidEmulatorEn.exe
+taskkill /f /t /im aow_exe.exe
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\367
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\367.ini
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\30
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\30.ini
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\188
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\188.ini
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\189
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\189.ini
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\4
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\4.ini
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\368
+del C:\Program Files\txgameassistant\AOW_Rootfs_100\0\368.ini
+
+pasue
